7f50ef5882c2195d8b2af640a6da7236ed73c572
Oscar Lara <olara@csumb.edu>
2021-02-12 21:06:54 -0800

    Started work on SavedJobsActivity

---
+++ app/src/main/java/com/codepath/synkae/cst438_proj1/SavedJobsActivity.java
@@ -0,0 +1,14 @@
+package com.codepath.synkae.cst438_proj1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedJobsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_jobs);
+    }
+}
